2796b709cfbf5a5d4b763549c8cc343cb65acddc
Chancellier Pierre-Jean <pierre-jean.chancellier@univ-lehavre.fr>
2024-01-30 17:27:47 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,32 @@
+<script setup>
+const date = ref(getToday());
+
+function getToday() {
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const month_f = month < 10 ? "0" + month : month;
+  return today.getFullYear() + "-" + month_f + "-" + today.getDate();
+}
+</script>
+
+<template>
+  <div>
+    <input type="date" v-model="date" />
+    <Calendar :date="date" />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  padding: 1rem;
+}
+
+input[type="date"] {
+  margin-bottom: 1rem;
+}
+</style>
---
+++ components/Calendar.client.vue
@@ -0,0 +1,212 @@
+<script setup>
+// https://github.com/vkurko/calendar
+import "@event-calendar/core/index.css";
+import Calendar from "@event-calendar/core";
+import TimeGrid from "@event-calendar/resource-time-grid";
+import Interaction from "@event-calendar/interaction";
+
+const props = defineProps({
+  date: String,
+});
+const calendar = ref();
+
+watch(
+  () => props.date,
+  (newValue) => {
+    calendar.value.setOption("date", newValue);
+  }
+);
+
+onMounted(async () => {
+  await nextTick();
+  calendar.value = new Calendar({
+    target: document.getElementById("ec"),
+    props: {
+      plugins: [TimeGrid, Interaction],
+      options: {
+        slotMaxTime: "19:00:00",
+        slotMinTime: "09:00:00",
+        // height: "500px",
+        slotHeight: 12,
+        view: "resourceTimeGridDay",
+        allDaySlot: false,
+        slotDuration: "00:15:00",
+        headerToolbar: { start: "", center: "", end: "" },
+        buttonText: (text) => ({
+          ...text,
+          today: "Aujourd'hui",
+          next: "Jour suivant",
+          prev: "Jour précédent",
+        }),
+        // Quand on clique sur un horaire du calendrier
+        dateClick: (info) => {
+          console.log(info);
+        },
+        // Quand on clique sur un event
+        eventClick: (info) => {
+          console.log(info);
+        },
+        // Après avoir déplacé un évènement
+        eventDrop: (info) => {
+          console.log(info);
+        },
+        // Après avoir modifié la durée d'un event
+        eventResize: (info) => {
+          console.log(info);
+        },
+        // Quand on clique pour créer un évènement
+        noEventsClick: (info) => {
+          console.log(info);
+        },
+        // Quand on change de date
+        datesSet: (info) => {
+          console.log(info);
+        },
+        datesAboveResources: true,
+        dayHeaderFormat: (date) => {
+          return date.toLocaleString("fr-FR", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          });
+        },
+        resources: [
+          { id: 1, title: "Studio A" },
+          { id: 2, title: "Studio B" },
+          { id: 3, title: "Studio C" },
+          { id: 4, title: "Studio D" },
+          { id: 5, title: "Studio E" },
+          { id: 6, title: "Studio F" },
+        ],
+        events: createEvents(),
+      },
+    },
+  });
+});
+
+function createEvents() {
+  let days = [];
+  for (let i = 0; i < 7; ++i) {
+    let day = new Date();
+    let diff = i - day.getDay();
+    day.setDate(day.getDate() + diff);
+    days[i] =
+      day.getFullYear() +
+      "-" +
+      _pad(day.getMonth() + 1) +
+      "-" +
+      _pad(day.getDate());
+  }
+
+  return [
+    {
+      start: days[0] + " 00:00",
+      end: days[0] + " 09:00",
+      resourceId: 1,
+      display: "background",
+    },
+    {
+      start: days[1] + " 12:00",
+      end: days[1] + " 14:00",
+      resourceId: 2,
+      display: "background",
+    },
+    {
+      start: days[2] + " 17:00",
+      end: days[2] + " 24:00",
+      resourceId: 1,
+      display: "background",
+    },
+    {
+      start: days[0] + " 10:00",
+      end: days[0] + " 14:00",
+      resourceId: 1,
+      title: "The calendar can display background and regular events",
+      color: "#FE6B64",
+    },
+    {
+      start: days[2] + " 09:00",
+      end: days[2] + " 13:00",
+      resourceId: 2,
+      title:
+        "Events can be assigned to resources and the calendar has the resources view built-in",
+      color: "#779ECB",
+    },
+    {
+      start: days[3] + " 14:00",
+      end: days[3] + " 20:00",
+      resourceId: 1,
+      title: "",
+      color: "#FE6B64",
+    },
+    {
+      start: days[3] + " 15:00",
+      end: days[3] + " 18:00",
+      resourceId: 1,
+      title: "Overlapping events are positioned properly",
+      color: "#779ECB",
+    },
+    {
+      start: days[5] + " 10:00",
+      end: days[5] + " 16:00",
+      resourceId: 2,
+      title: {
+        html: "You have complete control over the <i><b>display</b></i> of events…",
+      },
+      color: "#779ECB",
+    },
+    {
+      start: days[5] + " 14:00",
+      end: days[5] + " 19:00",
+      resourceId: 2,
+      title: "…and you can drag and drop the events!",
+      color: "#FE6B64",
+    },
+    {
+      start: days[5] + " 18:00",
+      end: days[5] + " 21:00",
+      resourceId: 2,
+      title: "",
+      color: "#B29DD9",
+    },
+  ];
+}
+
+function _pad(num) {
+  let norm = Math.floor(Math.abs(num));
+  return (norm < 10 ? "0" : "") + norm;
+}
+</script>
+
+<template>
+  <div style="max-width: 50%">
+    <div id="ec"></div>
+  </div>
+</template>
+
+<style>
+.ec-toolbar,
+.ec-header {
+  position: sticky;
+  z-index: 10;
+  background: white;
+}
+
+.ec-toolbar {
+  top: 0;
+  margin-bottom: 0;
+}
+.ec-header {
+  top: 0;
+}
+
+.ec-day {
+  text-transform: capitalize;
+}
+
+.ec-time,
+.ec-line {
+  height: 12px;
+}
+</style>
